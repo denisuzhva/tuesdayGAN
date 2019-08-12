ee9be4014e38a78c78341f61f0e2e9eeae9ad53c
mortarsynth <epirtochol@gmail.com>
2019-08-13 00:09:35 +0300

    prepared for 1st run

--- prepare_data/make_smol.py
+++ prepare_data/make_smol.py
@@ -2,6 +2,6 @@ import numpy as np
 
 
 dataset = np.load('../../DGAN_DATASET/sunn1_full.npy')
-dataset_smol = dataset[0:256, :, :, :]
+dataset_smol = dataset[0:(dataset.shape[0] // 128) * 128, :, :, :]
 print(dataset_smol.shape)
-np.save('../../DGAN_DATASET/sunn1_256.npy', dataset_smol)
+np.save('../../DGAN_DATASET/sunn1_1152.npy', dataset_smol)
